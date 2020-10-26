1488ac0e3029f7d8bae7bad7c31d2d2b9ea82a21
sparksparrow <50149745+sparksparrow@users.noreply.github.com>
2020-10-26 21:13:42 +0300

    'KivyMD_v1'

--- main.py
+++ main.py
@@ -1,74 +1,73 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.app import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivymd.app import MDApp
+from kivymd.uix.list import OneLineListItem
+from kivymd.uix.toolbar import MDToolbar
 from kivy.core.window import Window
 from kivy.properties import ObjectProperty
 import sqlite3
 import asyncio
 import websockets
 
-Builder.load_string('''
+kv_code = '''
+ScreenManager:
+    MainWindow:
+    Chat:    
+        
 <MainWindow>:
-    rows: 3
+    name: 'MainWindow'
+    BoxLayout:
+        orientation: "vertical"
+        MDToolbar:
+            title: 'Anonymous chat'
+            MDIconButton:
+                icon: './resources/plus.ico'
+                text: 'Hello'
+                pos_hint: {'center_x': 0.5, 'center_y': 0.5}
+                on_press: root.manager.current = 'Chat'
+        ScrollView:
+            MDList:
+                id: container
+                OneLineListItem:
+                    text: 'Item1'
+                OneLineListItem:
+                    text: 'Item2'
+        
+<Chat>:
+    name: 'Chat'
+    MDRectangleFlatButton:
+        text: 'Hello'
+        pos_hint: {'center_x': 0.5, 'center_y': 0.5}
+        on_press: root.manager.current = 'MainWindow'
+
+'''
 
 
-    AnchorLayout:
-        size_hint: 1, 0.10
-
-        BoxLayout:
-            cols: 2
-            orientation: 'horizontal'
-            Label:
-                font_name: './resources/appetite.ttf'
-                markup: True
-                text: 'Anonymous chat'
-                font_size: '35px'
-                canvas.before:
-                    Color:
-                        rgba: 0.5, 0.5, 1, 1
-                    Rectangle:
-                        pos: self.pos
-                        size: self.parent.size
-                valign: 'middle'
-                text_size: self.size
-            Button:
-                id: add_chat
-                size_hint: 0.2, 1
-                background_color: 0.1, 0.1, 0.1, 0.2
-                on_release: root.add_chat()
-                Image:
-                    allow_stretch: True
-                    size: self.parent.size
-                    pos: self.parent.pos
-                    source: './resources/add.png'
-
-    AnchorLayout:
-        padding: [15, 15, 15, 0]
-        cols:1
-        BoxLayout:
-            orientation: 'vertical'
-            Button:
-                size_hint: 1, 0.2
-                background_color: 0.5, 0.5, 1, 1
-            Button:
-                size_hint: 1, 0.2
-                background_color: 0.5, 0.5, 1, 1
-
-    AnchorLayout:
-        size_hint: 1,0.15
-''')
+class MainWindow(Screen):
+    # def add_chat(self):
+    pass  # Добавление чата (создается новая кнопка в поле для перехода на новый Layout
 
 
-class MainWindow(GridLayout):
-
-    def add_chat(self):
-        pass  # Добавление чата (создается новая кнопка в поле для перехода на новый Layout
+class Chat(Screen):
+    pass
 
 
-class ChatApp(App):
+sm = ScreenManager()
+sm.add_widget(MainWindow(name='MainWindow'))
+sm.add_widget(Chat(name='Chat'))
+
+
+class ChatApp(MDApp):
     def build(self):
-        return MainWindow()
+        sm = Builder.load_string(kv_code)
+        return sm
 
+    # def on_start(self):
+    #     for i in range(4):
+    #         items = OneLineListItem(text='Item '+str(i))
+    #         MainWindow.get_root_window(self).ids.container.add_widget(items)
 
 
 if __name__ == '__main__':
